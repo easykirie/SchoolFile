461456096f96821492286ac2e2a3be336dfb7f7e
Unknown <s2kmrs2@naver.com>
2018-09-28 16:42:02 +0900

    계산기+오름차순
    계산기는 완성, 오름차순 마무리 안됨

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float num1, num2, num3;
+            char mid;
+
+            
+
+            Console.WriteLine("연산을 할 두 수를 입력해주세요.");
+
+            num1 = float.Parse(Console.ReadLine());
+            num2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("어떤 연산자를 사용할지 입력하시오.");
+
+            mid = char.Parse(Console.ReadLine());
+
+            
+
+            if (mid == '+')
+            {
+                num3 = num1 + num2;
+            }
+            else if (mid == '-')
+            {
+                num3 = num1 - num2;
+            }
+            else if (mid == '*')
+            {
+                num3 = num1 * num2;
+            }
+            else num3 = num1 / num2;
+
+            Console.WriteLine("{0} {1} {2} = {3}", num1, mid, num2, num3);
+        }
+    }
+}
---
+++ Sort/Sort/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("몇개의 숫자를 입력할 것인지 입력하세요.");
+
+            int n = int.Parse(Console.ReadLine());
+
+            int[] nums = new int[n];
+            int[] results = new int[n];
+
+            Console.WriteLine("정렬할 숫자를 입력해주세요.");
+
+            for (int i=0;i<n;i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+
+                nums[i] = num;
+            }
+
+            Console.WriteLine("오름차순으로 정렬하고 하나씩 출력하세요.");
+
+            for(int x=0;x<n;x++)
+            {
+
+            }
+
+            
+            
+
+        }
+    }
+}
