f0b81fcc1010426e0471beab9c2e05de1956208f
Unknown <s2kmrs2@naver.com>
2018-10-26 16:30:40 +0900

    오브젝트풀링
    풀링에 대해 배움, 디자인패턴 검색해보는게 좋을듯

---
+++ PlaneInTheRiver (2)/Assets/Project/Scripts/GameCamera.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameCamera : MonoBehaviour {
+
+    public GameObject target;
+    public float verticalOffset;
+
+    void FixedUpdate()
+    {
+        if(target != null)
+        {
+            transform.position = new Vector3(0, target.transform.position.y + verticalOffset, transform.position.z);
+        }
+    }
+}
