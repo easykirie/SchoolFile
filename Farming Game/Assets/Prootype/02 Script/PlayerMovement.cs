434dc2ded98856dc4d0ded4bb327d3d7828ed135
Unknown <s2kmrs2@naver.com>
2018-12-14 16:34:54 +0900

    파밍겜(진반, 나)
    스테미너, 버프 and 디버프 수치 대입

---
+++ Farming Game/Assets/Prootype/02 Script/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float speed = 6f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetKey(KeyCode.LeftArrow) == true)
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.RightArrow) == true)
+        {
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.UpArrow) == true)
+        {
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.DownArrow) == true)
+        {
+            transform.Translate(Vector3.back * speed * Time.deltaTime);
+        }
+
+    }
+}
---
+++ Farming Game/Assets/Prootype/02 Script/PlayerStamina.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerStamina : MonoBehaviour {
+
+    public float playerstamina_max;
+    public float playerstamina_current;
+
+    Image img;
+
+    // Use this for initialization
+    void Start () {
+        playerstamina_current = playerstamina_max;
+        img = GetComponent<Image>();
+        float img_amount = img.fillAmount;
+        Color img_color = img.color;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Heart(float dmg)
+    {
+
+        playerstamina_current -= dmg;
+
+        img.fillAmount = playerstamina_current / playerstamina_max;
+    }
+}
+
